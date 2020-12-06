2f45f141362ad2130c931fe842b8247a7b1f0be8
claudine Kuradusenge <claudinekuradusenge2@gmail.com>
2020-12-06 07:16:29 +0200

    add changes

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wecode:coco@:5432/blog_web'
